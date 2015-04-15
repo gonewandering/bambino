052487d501f1bc0f04bbc19bf5286dbeb2d8b7c8
Morgan Loehr <molo@Morgans-MacBook-Air.local>
2015-04-15 09:51:45 -0700

    fixed page loop function

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,50 +11,32 @@ router.get('/:slug', function (req, res, next) {
 	var i = req.params.gid || 0;
 
 	if (req.params.slug == "editor") { next(); return false; }
-	db.Page.findOne({where: {'slug': req.params.slug}}).success(function (page) {
-		if (page) {
-		page.getGalleries().then(function (galleries) {
-			if (galleries && galleries.length > 0) { 
-
-				page.galleries = galleries;
-
-				galleries[i].getArtworks().then(function (artworks) { 
-					page.galleries[i].artworks = artworks;
-					page.gallery = page.galleries[i];
-
-					res.render(page.template || 'index', page);
-				});
-			} else { 
-
-				res.render(config.theme + page.template || 'index', page);
-			}
-		});
-		} else { 
-			next(); return false;
-		}
-	});
-});
-
-router.get('/', function (req, res, next) {
-	var i = req.params.gid || 0;
-
-	db.Page.findOne({where: {'slug': 'home'}}).success(function (page) {
-		if (page) { 
+	db.Page.findAll().success(function (pages) {
+		db.Page.findOne({where: {'slug': req.params.slug}}).success(function (page) {
+			if (page) {
 			page.getGalleries().then(function (galleries) {
 				if (galleries && galleries.length > 0) { 
 
 					page.galleries = galleries;
-					
 					galleries[i].getArtworks().then(function (artworks) { 
 						page.galleries[i].artworks = artworks;
 						page.gallery = page.galleries[i];
+						page.pages = pages;
 
-						res.render(config.theme + page.template || 'index', page);
+						res.render(page.template || 'index', page);
 					});
 				} else { 
-					res.render(page.template || 'index', page);
+					page.pages = pages;
+					res.render(config.theme + page.template || 'index', page);
 				}
 			});
-		} else { next(); return false; }
+			} else { 
+				next(); return false;
+			}
+		});
 	});
+});
+
+router.get('/', function (req, res, next) {
+	res.redirect('/home');
 });
