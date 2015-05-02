5c91ed7f8c8e744e660911755b5dbcf4ffb616a4
Lindsay Silver <lindsay@socialight.io>
2015-05-02 15:58:54 -0400

    Added private files

---
+++ config/config.js
@@ -0,0 +1,42 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    theme: 'emily/',
+    app: {
+      name: 'emilysilver'
+    },
+    port: process.env.PORT || 3000,
+  },
+
+  test: {
+    root: rootPath,
+    theme: 'emily/',
+    app: {
+      name: 'emilysilver'
+    },
+    port: process.env.PORT || 3000
+  },
+
+  production: {
+    root: rootPath,
+    theme: 'emily/',
+    app: {
+      name: 'emilysilver'
+    },
+    port: process.env.PORT || 3000
+  }
+};
+
+
+// This sets up the config object
+
+var conf = config[env];
+
+conf.db = conf.db || 'sqlite://localhost/' + conf.app.name + "-" + env;
+conf.storage = conf.root + '/data/' + conf.app.name + "-" + env;
+
+module.exports = conf;
---
+++ themes/editor/js/scripts.js
@@ -0,0 +1,153 @@
+jQuery(document).ready(function ($) {
+
+	// Dropbox Chooser
+	var options = {
+		linkType: 'direct',
+		extensions: ['.jpeg', '.jpg', '.png'],
+		success: function (files) {
+			$(".upload").val(files[0].link);
+
+			var square = files[0].thumbnailLink.replace("=75", "=256").replace("=fit", "=crop");
+			var display = files[0].thumbnailLink.replace("=75", "=1280").replace("=fit", "=fit_one_and_overflow");
+
+			$("[name='square']").attr("value", square);
+			$("[name='display']").attr("value", display);
+			$("[name='full']").attr("value", files[0].link);
+
+			$(".selected-image").attr("src", display);
+			$(".selected-image").removeClass("hidden");
+			$(".dropbox-button").addClass("hidden");
+
+			return true;
+		}
+	};
+
+	var button = Dropbox.createChooseButton(options);
+
+	$('[data-show]').on('click', function () {
+		var show = $(this).data('show');
+		$(".tab").hide();
+		$(show).fadeIn();
+		return false;
+	});
+
+	$('.dropbox').append(button);
+
+
+	// Set up sorting and drag drop for libraries
+
+	var sortUpdateGallery = function (e, dom) {
+		var type = dom.item.data("type"),
+			order = [], id = dom.item.data("id"),
+			pid = dom.item.data("page");
+
+		dom.item.parents('ul').children('li').each(function (d) {
+			order.push($(this).data('id'));
+		});
+
+		$.post("/editor/pages/" + pid + "/sort", {
+			order: order
+		}, function (res) {
+			console.log(res);
+		});
+	}
+
+	var sortUpdateArtwork = function (e, dom) {
+		var type = dom.item.data("type"),
+			order = [],
+			id = dom.item.data("id"),
+			gid = dom.item.data("gallery"),
+			pid = dom.item.data("page");
+
+		dom.item.parents('ul').children('li').each(function (d) {
+			order.push($(this).data('id'));
+		});
+
+		$.post("/editor/pages/" + pid + "/galleries/" + gid +"/sort", {
+			order: order
+		}, function (res) {
+			console.log(res);
+		});
+	}
+
+	$( ".sortable.galleries" ).sortable({
+		update: sortUpdateGallery,
+		revert: true,
+		handle: '.handle',
+		axis: 'y'
+	});
+
+	$( ".sortable.galleries" ).disableSelection();
+
+	$( ".sortable.artworks" ).sortable({
+		update: sortUpdateArtwork,
+		revert: true,
+		handle: '.handle',
+		axis: 'y'
+	});
+
+	$( ".sortable.artworks" ).disableSelection();
+
+
+	// Draggable
+
+	$( ".add-to-gallery.artwork" ).draggable({
+		revert: 'invalid',
+		helper: 'clone',
+		handle: '.drag-item'
+	});
+
+	$( ".add-to-page.gallery" ).draggable({
+		revert: 'invalid',
+		helper: 'clone',
+		handle: '.drag-item'
+	});
+
+	$( ".gallery" ).droppable({
+		hoverClass: 'active-drag',
+		accept: '.add-to-gallery',
+		drop: function( event, ui ) {
+			var p = $(this);
+			$.post("/editor/galleries/" + $(this).data("gid") + "/artworks/" + ui.draggable.data("aid"), function (res) {
+				p.find("label").text(p.find("label") + 1);
+			});
+		}
+	});
+
+	$( ".page" ).droppable({
+		hoverClass: 'active-drag',
+		accept: '.add-to-page',
+		drop: function( event, ui ) {
+			var p = $(this);
+			$.post("/editor/pages/" + $(this).data("pid") + "/galleries/" + ui.draggable.data("gid"), function (res) {
+				p.find("label").text(p.find("label") + 1);
+			});
+		}
+	});
+
+
+	// Delete functions
+
+	$('.delete-gallery').on('click', function () {
+		var art = $(this);
+		$.ajax({
+			url: "/editor/pages/" + $(this).data('id') + "/galleries/" + $(this).data('gid'),
+			method: 'DELETE',
+			complete: function (res) {
+				art.parents(".gallery").remove();
+			}
+		});
+	});
+
+	$('.delete-art').on('click', function () {
+		var art = $(this);
+		$.ajax({
+			url: "/editor/galleries/" + $(this).data('gid') + "/artworks/" + $(this).data('aid'),
+			method: 'DELETE',
+			complete: function (res) {
+				art.parents(".artwork").remove();
+			}
+		});
+	});
+
+});
