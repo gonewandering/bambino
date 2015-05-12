28f4e8575645f62570ad1ed3d68dd8e80409b8e7
Lindsay Silver <lindsay@socialight.io>
2015-05-12 13:42:23 -0400

    Added data and uploads

--- config/config.js
+++ config/config.js
@@ -3,39 +3,14 @@ var path = require('path'),
     env = process.env.NODE_ENV || 'development',
     _ = require('underscore');
 
-var config = {
-  development: {
-    root: rootPath,
-    theme: 'emily',
-    app: {
-      name: 'emilysilver'
-    },
-    port: process.env.PORT || 3000,
+var conf = {
+  root: rootPath,
+  theme: 'emily',
+  app: {
+    name: 'emilysilver'
   },
-
-  test: {
-    root: rootPath,
-    theme: 'emily',
-    app: {
-      name: 'emilysilver'
-    },
-    port: process.env.PORT || 3000
-  },
-
-  production: {
-    root: rootPath,
-    theme: 'emily',
-    app: {
-      name: 'emilysilver'
-    },
-    port: process.env.PORT || 3000
-  }
+  port: process.env.PORT || 3000,
 };
-
-
-// This sets up the config object
-
-var conf = config[env];
 
 conf.db = conf.db || 'sqlite://localhost/' + conf.app.name + "-" + env;
 conf.storage = conf.root + '/data/' + conf.app.name + "-" + env;
